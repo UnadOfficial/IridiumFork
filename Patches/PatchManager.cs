941d7be7997c0268b7e24cad77c650876c0d479d
Xbodwf <xbodwf@gmail.com>
2026-02-11 21:58:36 +0800

    feat: portal

--- Patches/PatchManager.cs
+++ Patches/PatchManager.cs
@@ -61,6 +61,7 @@ namespace Iridium.Patches
             _definitions.Add(new PatchDef(typeof(MiscPatches.ForceDifficultyUIPatch), () => Main.Settings.ui.forceDifficultyUI));
             _definitions.Add(new PatchDef(typeof(MiscPatches.CircleArcPatch), () => Main.Settings.ui.enableCircleArc));
             _definitions.Add(new PatchDef(typeof(MiscPatches.AutoplayTextPositionPatch), () => Main.Settings.ui.moveAutoplayText));
+            _definitions.Add(new PatchDef(typeof(CustomLevelIslandPatch), () => Main.Settings.ui.enableCustomLevelIsland));
             
             // Tail
             _definitions.Add(new PatchDef(typeof(MiscPatches.TailTweakPatch), () => Main.Settings.tail.enableTailTweak));
